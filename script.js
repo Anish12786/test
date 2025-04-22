c9e12eae1dcbb6f771b0f0fdbb39bc0177f01fc1
Anish12786 <anishsai59@gmail.com>
2025-04-23 00:51:25 +0545

    TO-Do list

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ console.log("hello");
 // git add .
 // git commit -m "message here"
 // git push
+
+function addTask() {
+    const newTask = document.createElement("li")
+    const tasklist = document.getElementById("tasklist")
+    tasklist.appendChild(newTask)
+    newTask.textContent = document.getElementById("inputTask").value
+    document.getElementById("inputTask").value = ""    // To make input section empty
+    deleteTask(newTask)
+}
+
+function deleteTask(newTask) {
+    const deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "Delete"
+    newTask.appendChild(deleteBtn)
+    deleteBtn.onclick = function(){
+        newTask.remove()
+    }
+}
